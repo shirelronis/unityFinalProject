a68240ababa5b22919fbc99da22c52a44dffbbb4
shirelronis <shirelbar100@gmail.com>
2022-07-08 17:27:16 +0300

    change version

---
+++ Assets/Scripts/StopGameScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StopGameScript : MonoBehaviour
+{
+    public Text winText;  
+    public Text looseText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        // Debug.Log(PlayerScript.isWin);
+        // if (PlayerScript.isWin) {
+        //     looseText.enabled = false;
+        //     winText.enabled=true;
+        // }
+        // else
+        // {
+        //      winText.enabled=false;
+        //     looseText.enabled = true;
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenu(){
+        SceneManager.LoadScene(0);
+    }
+}
