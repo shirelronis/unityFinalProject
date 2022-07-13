0eedf64f630f3aa2c215a11ef09dd3ebe630858c
shirelronis <shirelbar100@gmail.com>
2022-07-13 09:09:44 +0300

    add lives script

---
+++ Assets/Scripts/CollectLiveScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectLiveScript : MonoBehaviour
+{
+    public AudioClip collectSound;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == "Player") //We hit player
+        {
+            if (collectSound != null)
+            {
+                AudioSource.PlayClipAtPoint(collectSound, transform.position);
+            }
+            GameObject player = collision.gameObject;
+            PlayerScript pScript = player.GetComponent<PlayerScript>();
+            pScript.playerLives++;
+            Collect();
+        }
+    }
+
+    public void Collect(){
+        Destroy(gameObject);
+    }
+}
