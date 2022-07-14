91c7f9253e23bc2ecbfc0b0a2b3b94038aaa3f87
shirelronis <shirelbar100@gmail.com>
2022-07-14 12:45:17 +0300

    add cylinder remove lives

---
+++ Assets/Scripts/RemoveLiveScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveLiveScript : MonoBehaviour
+{
+    public AudioClip collectSound;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.tag == "Player") //We hit player
+        {
+            if (collectSound != null)
+            {
+                AudioSource.PlayClipAtPoint(collectSound, transform.position);
+            }
+            GameObject player = collision.gameObject;
+            PlayerScript pScript = player.GetComponent<PlayerScript>();
+            pScript.playerLives--;
+        }
+    }
+}
