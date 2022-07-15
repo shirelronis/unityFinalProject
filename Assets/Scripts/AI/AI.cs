fa7d09cec234266aea50a7aa5faa4342fbc83d35
shirelronis <shirelbar100@gmail.com>
2022-07-15 14:41:28 +0300

    ai script

---
+++ Assets/Scripts/AI/AI.cs
@@ -0,0 +1,38 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using StateStuff;
+
+// public class AI : MonoBehaviour
+// {
+//     public bool switchState = false;
+//     public float gameTimer;
+//     public int seconds = 0;
+
+//     public StateMachine<AI> stateMachine {get; set;}
+
+//     private void Start()
+//     {
+//         stateMachine = new StateMachine<AI>(this);
+//         stateMachine.ChangeState(FirstState.Instance);
+//         gameTimer = Time.time;
+//     }
+
+//     public Update()
+//     {
+//         if (Time.time > gameTimer + 1)
+//         {
+//             gameTimer.time = Time.time;
+//             seconds++;
+//             Debug.Log(seconds);
+//         }
+
+//         if (seconds == 5)
+//         {
+//             seconds = 0;
+//             switchState = !switchState;
+//         }
+
+//         stateMachine.Update();
+//     }
+// }
---
+++ Assets/Scripts/AI/FirstState.cs
@@ -0,0 +1,45 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using StateStuff;
+
+// public class FirstState : State<AI>
+// {
+//     private static FirstState _instance;
+
+//     private FirstState()
+//     {
+//         if(_instance != null)
+//             return;
+//         _instance = this;
+//     }
+
+//     public static FirstState Instance
+//     {
+//         get
+//         {
+//             if(_instance == null)
+//                 new FirstState();
+//             return _instance;
+//         }
+        
+//     }
+
+//     public override void EnterState(AI _owner)
+//     {
+//         Debug.Log("Entering first state");
+//     }
+
+//     public override void ExitState(AI _owner)
+//     {
+//         Debug.Log("Exitin first state");
+//     }
+
+//     public override void UpdateState(AI _owner)
+//     {
+//         if(_owner.switchState)
+//         {
+//             _owner.stateMachine.ChangeState(SecondState.Instance);
+//         }
+//     }
+// }
