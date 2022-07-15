727512a6c568a2832f015ba4f063de646fabee64
Hadas <hadasadam8@gmail.com>
2022-07-15 14:51:53 +0300

    instructions

---
+++ Assets/Scripts/InstructionScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionScript : MonoBehaviour
+{
+    public GameObject image;
+
+    void Start()
+    {
+        image.SetActive(false); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            image.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            image.SetActive(false);
+        }
+    }
+}
