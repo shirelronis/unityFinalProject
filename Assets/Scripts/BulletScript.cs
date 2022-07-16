99831b2ae72136474304c736d0d6745cc99d952a
Hadas <hadasadam8@gmail.com>
2022-07-16 10:07:52 +0300

    add ghostler master and how to manage with it

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BulletScript : MonoBehaviour
 {
     public static int BULLET_HIT = 20;
     public int bulletSpeed = 10;
+    private int masterNeededShootings = 3; 
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +30,22 @@ public class BulletScript : MonoBehaviour
             ScoringSystemScript.score += BULLET_HIT;
             other.transform.position = new Vector3(Random.Range(-8f, 8), 6, 0);
             Destroy(other);
+        }
+
+        if (other.gameObject.tag == "GhostlerMaster")
+        {
+            masterNeededShootings--;
+            PlayerScript.killedGhostlers++;
+            ScoringSystemScript.score += BULLET_HIT;
+            other.transform.position = new Vector3(Random.Range(-8f, 8), 6, 0);
+
+            if(masterNeededShootings == 0)
+            {
+                Destroy(other);
+
+                //Winning The Game
+                SceneManager.LoadScene(2);
+            }
         }
     }
 }
--- Assets/Scripts/ScoringSystemScript.cs
+++ Assets/Scripts/ScoringSystemScript.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 
 public class ScoringSystemScript : MonoBehaviour
 {
-    private const int WINNING_SCORE = 200;
     public static bool isWin = true;
 
     public static int score = 0;
@@ -15,12 +14,6 @@ public class ScoringSystemScript : MonoBehaviour
         {
             PlayerScript.playerLives--;
             score = 0;
-        }
-
-        if (score >= WINNING_SCORE)
-        {
-            //winning
-            SceneManager.LoadScene(2);
         }
     }
 
