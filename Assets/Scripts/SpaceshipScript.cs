6395c00b29dec0401902f25a32379a777daa3a55
Hadas <hadasadam8@gmail.com>
2022-07-17 19:28:42 +0300

    build 2

--- Assets/Scripts/SpaceshipScript.cs
+++ Assets/Scripts/SpaceshipScript.cs
@@ -6,27 +6,37 @@ using UnityEngine.SceneManagement;
 
 public class SpaceshipScript : MonoBehaviour
 {
+    Vector3 startPosition;
+    bool didSceneEnd = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(didSceneEnd)
+        {
+            gameObject.transform.position = startPosition;
+            didSceneEnd = false;
+        }
+
         if(gameObject.transform.position.z > 60 || gameObject.transform.position.y < -4)
         {
             //losing
+            didSceneEnd = true;
             SceneManager.LoadScene(5);
         }
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
+            didSceneEnd = true;
             NextLevel();
         }
     }
